a9ff068d96d77711d29f15a75dc5c5ba58662616
DragonRicky21 <77455441+DragonRicky21@users.noreply.github.com>
2022-07-07 02:23:03 -0400

    Add files via upload

---
+++ Cédula de Identidad. Windows forms. + JSON/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Cédula_de_Identidad.Windows_form
+{
+    public partial class Form1 : Form
+    {
+        public bool Adding { get; set; } = true;
+        public Form1()
+        {
+            InitializeComponent();
+            if (this.dgvUsuarios.SelectedRows.Count == 0)
+            {
+                btnBorrar.Enabled = false;
+            }
+        }
+
+
+        List<Usuario> Usuarios = new List<Usuario>();
+
+
+        private void btnCrear_Click_1(object sender, EventArgs e)
+        {
+            btnCrear.Enabled = true;
+            btnActualizar.Enabled = true;
+            btnBorrar.Enabled = true;
+            btnSubirFotoPerfil.Enabled = true;
+            gbDatos.Enabled = true;
+        }
+        private void btnActualizar_Click_1(object sender, EventArgs e)
+        {
+            ActualizarUsuario();
+        }
+        
+
+        private void ActualizarUsuario()
+        {
+            var json = string.Empty;
+            var usuarioList = new List<Usuario>();
+            var pathFile = $"{AppDomain.CurrentDomain.BaseDirectory}\\Usuarios.json";
+
+            if (File.Exists(pathFile))
+            {
+                json = File.ReadAllText(pathFile, Encoding.UTF8);
+                usuarioList = JsonConvert.DeserializeObject<List<Usuario>>(json);
+            }
+
+            var Usuario1 = new Usuario();
+            if (Adding)
+            {
+                Usuario1 = new Usuario
+                {
+                    NombreUsuario = this.txtBoxNombre.Text,
+                    LugarNacimiento = this.txtBoxLugarNac.Text,
+                    Cedula = this.txtBoxCedula.Text,
+                    FechaNacimiento = this.dtpFechaNacimiento.Value,
+                    Nacionalidad = this.txtBoxNacionalidad.Text,
+                    Sexo = this.cbSexo.Text,
+                    Sangre = this.cbSangre.Text,
+                    EstadoCivil = this.cbEstadoCivil.Text,
+                    Ocupacion = this.txtBoxOcupacion.Text,
+                    FechaExpiracion = this.dtpFechaExpiracion.Value,
+                    FotoPerfil = this.pbFotoPerfil.Image,
+
+
+                };
+            }
+
+            usuarioList.Add(Usuario1);
+
+            json = JsonConvert.SerializeObject(usuarioList);
+
+            var sw = new StreamWriter(pathFile, false, Encoding.UTF8);
+            sw.Write(json);
+            sw.Close();
+
+            MessageBox.Show("CIUDADANO AÑADIDO");
+
+            LimpiarControles();
+
+
+            GetUsuarios();
+
+        }
+        private void GetUsuarios()
+        {
+            var pathFile = $"{AppDomain.CurrentDomain.BaseDirectory}\\Usuarios.json";
+            var usuarioList = new List<Usuario>();
+
+            if (File.Exists(pathFile))
+            {
+                var json = File.ReadAllText(pathFile, Encoding.UTF8);
+                usuarioList = JsonConvert.DeserializeObject<List<Usuario>>(json);
+            }
+
+            dgvUsuarios.DataSource = null;
+            dgvUsuarios.DataSource = usuarioList;
+        }
+
+        private void LimpiarControles()
+        {
+            pbFotoPerfil.Image = null;
+            dtpFechaNacimiento.Value = DateTime.Today;
+            dtpFechaExpiracion.Value = DateTime.Today;
+
+            foreach (Control X in gbDatos.Controls)
+            {
+                if (X is TextBox )
+                {
+                    X.Text = string.Empty;
+                }
+            }
+
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+
+        {
+           
+        }
+
+        private void btnSubirFotoPerfil_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image File(*.png; *.jpg; *.jpeg; *.gif; *.bmp)|*.png; *.jpg; *.jpeg; *.gif; *.bmp";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                pbFotoPerfil.Image = new Bitmap(ofd.FileName);
+
+            }
+
+        }
+
+     
+        private void dgvUsuarios_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewSelectedCellCollection selection = this.dgvUsuarios.SelectedCells;
+            btnBorrar.Enabled = true;
+
+            this.txtBoxNombre.Text = Convert.ToString(selection[0].Value);
+            this.txtBoxCedula.Text = Convert.ToString(selection[1].Value);
+            this.txtBoxLugarNac.Text = Convert.ToString(selection[2].Value);
+            this.dtpFechaNacimiento.Value = Convert.ToDateTime(selection[3].Value);
+            this.txtBoxNacionalidad.Text = Convert.ToString(selection[4].Value);
+            this.cbSexo.Text = Convert.ToString(selection[5].Value);
+            this.cbSangre.Text = Convert.ToString(selection[6].Value);
+            this.cbEstadoCivil.Text = Convert.ToString(selection[7].Value);
+            this.txtBoxOcupacion.Text = Convert.ToString(selection[8].Value);
+            this.dtpFechaExpiracion.Value = Convert.ToDateTime(selection[9].Value);
+            this.pbFotoPerfil.Image = (Image)selection[10].Value;
+
+        }
+
+        private void txtBoxCedula_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbFotoPerfil_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbSexo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBorrar_Click_1(object sender, EventArgs e)
+        {
+            LimpiarControles();
+
+            DataGridViewRow fila = this.dgvUsuarios.SelectedRows[0];
+
+            Usuarios.RemoveAt(fila.Index);
+
+            GetUsuarios();
+            btnBorrar.Enabled = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        private void dgvUsuarios_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+
+    public class Usuario
+    {
+
+        public string NombreUsuario { get; set; }
+        public string Cedula { get; set; }
+
+        public string LugarNacimiento { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Nacionalidad { get; set; }
+        public string Sexo { get; set; }
+        public string Sangre { get; set; }
+        public string EstadoCivil { get; set; }
+
+        public string Ocupacion { get; set; }
+        public DateTime FechaExpiracion { get; set; }
+        public Image FotoPerfil { get; set; }
+        
+
+
+    }
+
+
+}
